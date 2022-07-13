ec5a5c2c82a049e9d8c79fa8bd35baa4c0de4a84
senik <sebastienwenzel@gmail.com>
2022-07-13 19:14:27 +1100

    add file product

--- front/js/product.js
+++ front/js/product.js
@@ -8,9 +8,9 @@ fetch("http://localhost:3000/api/products/" + id)
 // une reponse de la bonne réception du lien + ajout de identifiant du produit
 .then ((response) => {
     return response.json(); 
-    
+    // traduit en JSON : tableau contenant des objets
 })
-// traduit en JSON : tableau contenant des objets
+
 .then((product) => {
     const image = document.querySelector('.item__img');
     const titre = document.querySelector('#title');
@@ -29,8 +29,9 @@ fetch("http://localhost:3000/api/products/" + id)
         //Affectation après addition (+=) :permet de concaténer les possibilités des options de couleur 
 }
 //Boucle for permettant de parcourir un tableau 
-//console.log(product);
-})
+addCart();
+
+});
 // On injecte les infos dans le HTML
 
 
@@ -40,55 +41,62 @@ fetch("http://localhost:3000/api/products/" + id)
 
 //--Ajout du produit et ses infos au localstorage--//
 
-const btnAdd = document.querySelector('#addToCart');
-const color = document.querySelector('#colors');
-const quantity = document.querySelector('#quantity');
 
 
-btnAdd.addEventListener('click', (e) => {
-// Au clic, l'évènement s'effectue si les champs sont renseignés    
+function addCart(){
+    
+    //Définition des champs à renseigner
+    const btnAdd = document.querySelector('#addToCart');
+    const color = document.querySelector('#colors');
+    const quantity = document.querySelector('#quantity');
 
-    // LocalStorage
+    btnAdd.addEventListener('click', () => {
+        //color.value !== "" && quantity.value > 1 && quantity.value < 100)
+        if (!color.value){
+            alert('veuillez choisir une couleur');
+            return;
+        }
+        if(quantity.value < 1 || quantity.value > 100) {
+            alert('veuillez choisir entre 1 et 100');
+            return
 
-    function saveProduct(lounge){
-        localStorage.setItem("Users", JSON.stringify(lounge));
-        //JSON.stringify transforme tableau objet en chaine de caractère        
-    }
-    //AJOUT PRODUUIT DANS LOCALTSTORAGE
-
-    function getProduct(){
-        let lounge =  localStorage.getItem("Users");  
-        if (color.value !== "" && quantity != 0 && quantity < 100) {            
+        }
+                 
             let productObjet = {
                 id:id,
                 color: color.value,
-                quantity:quantity.value
+                quantity:parseInt(quantity.value)
+                //changer chaîne de caractère to number
             } 
-            // Création d'un objet produit
-            return JSON.parse(lougne);
-            //JSON.parse transforme chaine de caractère en tableau ou objet 
-        }
-        function addCart(productObjet) {
-            let shoppingCart = getProduct();
-            let foundProduct = lounge.find(p => p.id == productObjet.id);
-            //cherche élément sur un tableau par rapport à une condition
-            if (foundProduct != undefined){
-                foundProduct.quantity++;
-            } else {
-                productObjet = 1;  
-                productObjet.push(produc);
+            // Mise à disposition du localStorage si existant
+            let productLocalStorage = JSON.parse(localStorage.getItem("users"));
+
+            // Comportement si il n'y a pas de localStorage (il n'a ni valeur ni type défini : donc null)
+            if (productLocalStorage == null) {
+                productLocalStorage = [];
+                productLocalStorage.push(productObjet);
                 // .push action de pousser dans le tableau(panier)
+            } else {
+                // Comportement si il existe des données dans le localStorage
+
+                // Condition si le produit comporte le même Id et la même couleur. Méthode find dans le localStorage et comparaison avec les valeurs de l'objet userProductArray
+
+                let foundProduct = productLocalStorage.find(p => p.id == productObjet.id && p.color == productObjet.color);
+                //cherche élément sur un tableau par rapport à une condition .find
+                if (foundProduct != undefined){
+                    foundProduct.quantity += productObjet.quantity ;
+                    
+                } else {
+                    //productLocalStorage = 1 ;
+                    productLocalStorage.push(productObjet);
+                }
+               
             }
-            saveProduct(lounge);
-        }
-        } 
-    //PRENDRE PRODUIT DANS LOCALSTORAGE
+            localStorage.setItem("users", JSON.stringify(productLocalStorage));
+            alert("Le produit ajouté");
+    });    
+};
 
-   
-    //AJOUT PRODUIT DANS PANIER    
-
-addCart();
-});
 
 
 
